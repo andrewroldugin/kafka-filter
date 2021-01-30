e36954b30512dfde7a44f853e852802376667200
Andrew Roldugin <andrew.roldugin@gmail.com>
2021-01-30 19:15:45 +0200

    Web service stub.

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [compojure "1.6.2"]
+                 [http-kit "2.5.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-json "0.5.0"]]
   :main ^:skip-aot kafka-filter.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/kafka-filter/core.clj
+++ src/kafka-filter/core.clj
@@ -1,7 +1,51 @@
 (ns kafka-filter.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+            [ring.util.response :refer [response bad-request not-found created]])
   (:gen-class))
 
+(defn parse-int [number-string]
+  (try (Integer/parseInt number-string)
+       (catch Exception e nil)))
+
+(defn get-filter [req]
+  (let [params (:params req)]
+    (cond
+      (empty? params) (response {:msg "TODO: All filters"})
+      (:id params) (if-let [id (parse-int (:id params))]
+                     (response {:msg (str "TODO: Print filter " id " content")})
+                     (bad-request {:msg "Filter id must be integer"}))
+      :else (bad-request {:msg (str "Unsupported query: " (:query-string req))}))))
+
+(defn add-filter [req]
+  (pp/pprint req)
+  (response {:id 2464}))
+
+(defn remove-filter [req]
+  (pp/pprint req)
+  (let [body (:body req)]
+    (cond
+      (map? body) (response {:id "new-id" })
+      :else (bad-request {:msg "Unexpected param"}))))
+
+(defroutes app-routes
+  (GET "/filter" [] get-filter)
+  (POST "/filter" [] add-filter)
+  (DELETE "/filter" [] remove-filter)
+  (route/not-found (fn [_] (not-found {:msg "Page not found"}))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    (server/run-server
+     (-> #'app-routes
+         (wrap-json-response)
+         (wrap-json-body {:keywords? true :bigdecimals? true})
+         (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false)))
+     {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
